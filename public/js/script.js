6b5663bb3143f3595c0575fcc79954315076c52b
SantiagoCortina <cortina.zapfe@gmail.com>
2021-12-13 22:10:06 -0600

    Done

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,45 @@
+//const { default: axios } = require("axios");
+
 document.addEventListener(
   "DOMContentLoaded",
   () => {
+    let fromDate = document.getElementById('fromDate')
+    let toDate = document.getElementById('toDate')
+
+    function getData(){
+      const APIURI = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate.value}&end=${toDate.value}&currency=USD`
+      axios.get(APIURI)
+        .then(response => {
+          console.log(response.data)
+          setData(response.data)
+        })
+        .catch(error => {
+          console.log('Error amigos: ', error)
+        })
+    }
+
+    getData();
+
+    function setData(stockData){
+      const canvas = document.getElementById('myChart');
+      const ctx = canvas.getContext('2d');
+      chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              label: 'Bitcoin Price',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: stockData.bpi
+            }
+          ]
+        }
+      })
+    }
+
     console.log("lab-financial-data-graphing JS imported successfully!");
   },
   false
 );
+
